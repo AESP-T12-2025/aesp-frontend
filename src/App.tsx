70411267389bc0adc5ba182ca29cabef9ab0b0e9
huydd7998 <huydd7998@ut.edu.vn>
2026-01-05 16:26:22 +0700

    lam lai

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from 'react-router-dom'; 
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './pages/Home';
+import Home from './app/page';
 import PracticePage from './pages/PracticePage';
-import ScenarioListPage from './pages/ScenarioListPage';
+import ScenarioListPage from './app/scenarios/page';
 import './App.css';
 
 function App() {
   return (
-    // Xóa thẻ <Router> ở đây
+    
     <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
       <Header />
       <main style={{ flex: 1, padding: '20px' }}>
